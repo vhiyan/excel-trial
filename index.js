699d3ad3e0e6d352f5c8e64f3d058aab5f192d80
vhiyan <kristiawan.devianto@gmail.com>
2022-09-30 00:59:36 +0700

    bisa baca tp blm compare

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const readXlsxFile = require('read-excel-file/node')
 const fs = require("fs");
+const assert = require('assert');
 
-
+const pathExcel = `29 SEPTEMBER 2022.xlsx`;
+const pathTxt = '/home/vian/daftarJig30Sept.txt';
 
 // // File path.
 let katashiki=[];
@@ -14,24 +16,46 @@ let indexGroup=0;
 let katashikiJsonPrev={model:0,warna:0,type:0};
 let counterKatashiki;
 
+let katashikiFromLog;
+let flattenKatashikiFromLog;
+let groupKatashikiLog=[];
+let indexGroupLog=0;
+let counterKatashikiLog;
+let katashikiFromLogPrev={model:0,warna:0,type:0};
+
+
 const model={
-  'D40L':1,
-  'D40D':4,
-	'JUMP':4,
-  'SWING':4,
-  'CUT':4,	
-  'D17D':5,	
-  'D26':6,	
-  'T26':7,
-  'LOW':8,
+  'D40L'  :1,
+  'D40D'  :4,
+	'JUMP'  :4,
+  'SWING' :4,
+  'CUT'   :4,	
+  'D17D'  :5,	
+  'D26'   :6,	
+  'T26'   :7,
+  'LOW'   :8,
   'TOYOTA':9,
-  
-	'D12L':2,
-  'AERO':2,
-  
-	'HIGHT':3
+	'D12L'  :2,
+  'AERO'  :2,
+	'HIGHT' :3
   // 'RR':40
 
+}
+
+const modelLog={
+  1 :'D40L'  ,
+  4 :'D40D'  ,
+  4 :'JUMP'  ,
+  4 :'SWING' ,
+  4 :'CUT'   ,
+  5 :'D17D'  ,
+  6 :'D26'   ,
+  7 :'T26'   ,
+  8 :'LOW'   ,
+  9 :'TOYOTA',
+  2 :'D12L'  ,
+  2 :'AERO'  ,
+  3 :'HIGHT' 
 }
 
 const warna={
@@ -51,6 +75,22 @@ const warna={
 	'R75': 17
 }
 
+const warnaLog={
+  1  :'1G3',
+  2  :'T23',
+  4  :'1E7',
+  6  :'X12',
+  7  :'3Q3',
+  9  :'W09',
+  11 :'R71',
+  12 :'4T3',
+  14 :'X13',
+  16 :'X09',
+  19 :'S28',
+  20 :'P20',
+  21 :'G64',
+  17 :'R75'
+}
 
 
 // convert katashiki to JSON model, warna, dan type
@@ -63,7 +103,7 @@ function convert(item){
   
   if(!model[data[0]]){
     if(data[0]==='RR'){
-      return {"model":40,"warna":warna[data[3]],"type":3}
+      return {"model":40,"color":warna[data[3]],"type":3}
     }
       else return item
   }
@@ -74,46 +114,100 @@ function convert(item){
   
   data[2]==='FR'?typeKatashiki=2:typeKatashiki=1;  //jika ada tambahan FR-FR type jd FR-FR
     
-  return {"model":model[data[0]],"warna":warna[data[1]],"type":typeKatashiki}
+  return {"model":model[data[0]],"color":warna[data[1]],"type":typeKatashiki}
   }
 
-readXlsxFile(fs.createReadStream('20 SEPTEMBER 2022.xlsx')).then((rows) => {
-  // `rows` is an array of rows
-  // each row being an array of cells.
-  for (let index = 0; index < 7; index++) {
-    katashiki.push(rows[index+7]);
+
+function readall (stream)
+{
+  return new Promise ((resolve, reject) => {
+    const chunks = [];
+    stream.on ('error', (error) => reject (error));
+    stream.on ('data',  (chunk) => chunk && chunks.push (chunk));
+    stream.on ('end',   ()      => resolve (Buffer.concat (chunks)));
+  });
+}
+
+
+  function readfile (filename)
+  {
+    return readall (fs.createReadStream (filename));
   }
 
-  for (let indexBaris = 0; indexBaris < 7; indexBaris++) {
-    
-    for (let i = 1; i < 65; i++) {
-      if(katashiki[indexBaris][i]!=null)katashikiConv.push(katashiki[indexBaris][i])
+  async function readExcelFile()
+  {
+  await readXlsxFile(fs.createReadStream(pathExcel)).then((rows) => {
+    // `rows` is an array of rows
+    // each row being an array of cells.
+    for (let index = 0; index < 7; index++) {
+      katashiki.push(rows[index+7]);
     }
-  }
-  katashikiConv.filter(Number); //remove null element 
   
-  // groupKatashiki[indexGroup].push(katashikiConv[0])
-  // groupKatashiki[`1-${katashikiConv[1]}`] = convert(katashikiConv[1])
-
-  console.log(katashikiConv.length);
-  
-  for (let indexKatashiki = 0; indexKatashiki < katashikiConv.length; indexKatashiki++) {
-    const katashikiJson = convert(katashikiConv[indexKatashiki]);
-
-
-    
-    if(katashikiJson.model)
-    {
-      if(katashikiJson.model!=katashikiJsonPrev.model||katashikiJson.warna!=katashikiJsonPrev.warna|| katashikiJson.type!=katashikiJsonPrev.type){
-        indexGroup++;
-        counterKatashiki=0;
+    for (let indexBaris = 0; indexBaris < 7; indexBaris++) {
+      
+      for (let i = 1; i < 65; i++) {
+        if(katashiki[indexBaris][i]!=null)katashikiConv.push(katashiki[indexBaris][i])
       }
     }
-    counterKatashiki++;
-    groupKatashiki[`${indexGroup}-${katashikiConv[indexKatashiki]}`] = {...katashikiJson,counter:counterKatashiki};
-    katashikiJsonPrev = katashikiJson;
-  }
-    console.log(groupKatashiki);
-
+    katashikiConv.filter(Number); //remove null element 
+    
+    for (let indexKatashiki = 0; indexKatashiki < katashikiConv.length; indexKatashiki++) {
+      const katashikiJson = convert(katashikiConv[indexKatashiki]);
+  
+  
       
-})
+      if(katashikiJson.model)
+      {
+        if(katashikiJson.model!=katashikiJsonPrev.model||katashikiJson.color!=katashikiJsonPrev.color|| katashikiJson.type!=katashikiJsonPrev.type){
+          indexGroup++;
+          counterKatashiki=0;
+        }
+      }
+      counterKatashiki++;
+      groupKatashiki[`${indexGroup}`] = {...katashikiJson,counter:counterKatashiki};
+      katashikiJsonPrev = katashikiJson;
+    }
+  })
+  // console.log(groupKatashiki);
+  return groupKatashiki;   
+  }
+
+  async function readTxt(){
+    let content = await readfile(pathTxt).catch ((e) => {})
+    let baris = content.toString().split(`\n`);
+    for (let indexLog = 0; indexLog < baris.length; indexLog++) {
+      const tempKatashiki = baris[indexLog].split(`\t`);
+      if(tempKatashiki[1])
+      {
+        katashikiFromLog = JSON.parse(tempKatashiki[1]);
+        delete katashikiFromLog.counter;
+        
+        if(katashikiFromLog.model!=katashikiFromLogPrev.model||katashikiFromLog.warna!=katashikiFromLogPrev.warna||katashikiFromLog.type!=katashikiFromLogPrev.type)
+        {
+          indexGroupLog++;
+          counterKatashikiLog=0;  
+        }
+        counterKatashikiLog++;
+        groupKatashikiLog[`${indexGroupLog}`] = {...katashikiFromLog,counter:counterKatashikiLog};
+        katashikiFromLogPrev = katashikiFromLog;
+      }
+    }
+    // console.log(groupKatashikiLog);
+  return groupKatashikiLog;
+  }
+
+
+  describe('compare', function() {
+    it('same', async function() {
+      const pattern  = await readExcelFile();
+      const plc = await readTxt();
+      console.log(plc.length);
+      it.each(plc,'tes',model,...)
+      for (let index = 0; index < plc.length; index++) {
+        if(pattern[index]&&pattern[index]){
+          assert.equal(pattern[index].model,plc[index].model);
+        }
+      }
+    });
+  });  
+
